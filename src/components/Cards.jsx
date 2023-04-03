7772c42ddf99f5f0935b6432f4134dba26021e76
Jorge Alfredo Lozano <117942848+JorgeAlfredoLozano@users.noreply.github.com>
2023-04-03 14:44:29 -0300

    Primer commit

---
+++ src/components/Cards.jsx
@@ -0,0 +1,25 @@
+import Card from './Card';
+
+export default function Cards({characters}) { //[{...},{...}]
+   return (
+      <div>
+         {
+            characters.map((character) => { //puedo hacer destructuring si quisiera
+               return (
+                  <Card
+                  key={character.id}
+                  name={character.name}
+                  status={character.status}
+                  species={character.species}
+                  gender={character.gender}
+                  origin={character.origin.name}
+                  image={character.image}
+                  onClose={() => window.alert('Emulamos que se cierra la card')}
+                  />
+               );
+            })
+         }
+         
+      </div>
+   ) ;
+}
